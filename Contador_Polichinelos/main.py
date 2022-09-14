d29bbdb940ef7c52b056ca6ef31ebf242523dacd
Andrietti <meier.gabriel@hotmail.com>
2022-09-14 09:23:24 -0300

    Aprimoramento do código
    Retirando variáveis desnecessárias que foram inutilizadas com as mudanças

--- Contador_Polichinelos/main.py
+++ Contador_Polichinelos/main.py
@@ -14,7 +14,7 @@ layout = [
     [sg.Image(filename='C:\\Users\\Usuario\\Desktop\\html\\Aula-Jonas\\reconhecedorDePolichinelos\\Contador_Polichinelos\\imagens\\healthy.png', )],
     [sg.Text('Seja bem vindo ao OPEN-TRAINER')],
     [sg.Button('Contador de polichinelos', key='polichinelos'), sg.Button('Contador de agachamentos', key='agachamento')],
-    [sg.Button('Contador de flexões', key='flexao'), sg.Button('Contador de rosca direta')]
+    [sg.Button('Contador de flexões', key='flexao'), sg.Button('Contador de rosca direta', key='rosca')]
 ]
 
 window = sg.Window('Open-trainer', layout= layout, element_padding=(10, 10))
@@ -30,6 +30,8 @@ while True:
         pol.agachamento()
     elif event == 'flexao':
         pol.flexao()
+    elif event == 'rosca':
+        pol.rosca_direta()
     else:
         sg.popup('Lamento, essa função ainda não está completa :(')
 
--- Contador_Polichinelos/polichinelos.py
+++ Contador_Polichinelos/polichinelos.py
@@ -92,25 +92,29 @@ def polichinelos():
 
             # Coordenadas de cada ponto dos pés e mãos
             peDY = int(points.landmark[pose.PoseLandmark.RIGHT_FOOT_INDEX].y*h)
+
             peDX = int(points.landmark[pose.PoseLandmark.RIGHT_FOOT_INDEX].x*w)
+
             peEY = int(points.landmark[pose.PoseLandmark.LEFT_FOOT_INDEX].y*h)
+
             peEX = int(points.landmark[pose.PoseLandmark.LEFT_FOOT_INDEX].x*w)
+
             moDY = int(points.landmark[pose.PoseLandmark.RIGHT_INDEX].y*h)
+
             moDX = int(points.landmark[pose.PoseLandmark.RIGHT_INDEX].x*w)
+
             moEY = int(points.landmark[pose.PoseLandmark.LEFT_INDEX].y*h)
+
             moEX = int(points.landmark[pose.PoseLandmark.LEFT_INDEX].x*w)
+
             quEY = int(points.landmark[pose.PoseLandmark.LEFT_HIP].y*h)
+
             quEX = int(points.landmark[pose.PoseLandmark.LEFT_HIP].x*w)
+
             quDY = int(points.landmark[pose.PoseLandmark.RIGHT_HIP].y*h)
+
             quDX = int(points.landmark[pose.PoseLandmark.RIGHT_HIP].x*w)
-            omDY = int(points.landmark[pose.PoseLandmark.RIGHT_SHOULDER].y*h)
-            omDX = int(points.landmark[pose.PoseLandmark.RIGHT_SHOULDER].x*w)
-            omEY = int(points.landmark[pose.PoseLandmark.LEFT_SHOULDER].y*h)
-            omDX = int(points.landmark[pose.PoseLandmark.LEFT_SHOULDER].x*w)
-            coDY = int(points.landmark[pose.PoseLandmark.RIGHT_ELBOW].y*h)
-            coDX = int(points.landmark[pose.PoseLandmark.RIGHT_ELBOW].x*w)
-            coEY = int(points.landmark[pose.PoseLandmark.LEFT_ELBOW].y*h)
-            coEX = int(points.landmark[pose.PoseLandmark.LEFT_ELBOW].x*w)
+
             
 
 
@@ -218,21 +222,15 @@ def agachamento():
 
             # Coordenadas de cada ponto dos pés e mãos
             peDY = int(points.landmark[pose.PoseLandmark.RIGHT_FOOT_INDEX].y*h)
+
             peDX = int(points.landmark[pose.PoseLandmark.RIGHT_FOOT_INDEX].x*w)
+
             peEY = int(points.landmark[pose.PoseLandmark.LEFT_FOOT_INDEX].y*h)
+
             peEX = int(points.landmark[pose.PoseLandmark.LEFT_FOOT_INDEX].x*w)
-            quDY = int(points.landmark[pose.PoseLandmark.RIGHT_HIP].y*h)
-            quDX = int(points.landmark[pose.PoseLandmark.RIGHT_HIP].x*w)
+            
             quEY = int(points.landmark[pose.PoseLandmark.LEFT_HIP].y*h)
-            quEX = int(points.landmark[pose.PoseLandmark.LEFT_HIP].x*w)
-            omDY = int(points.landmark[pose.PoseLandmark.RIGHT_SHOULDER].y*h)
-            omDX = int(points.landmark[pose.PoseLandmark.RIGHT_SHOULDER].x*w)
-            omEY = int(points.landmark[pose.PoseLandmark.LEFT_SHOULDER].y*h)
-            omDX = int(points.landmark[pose.PoseLandmark.LEFT_SHOULDER].x*w)
-            coDY = int(points.landmark[pose.PoseLandmark.RIGHT_ELBOW].y*h)
-            coDX = int(points.landmark[pose.PoseLandmark.RIGHT_ELBOW].x*w)
-            coEY = int(points.landmark[pose.PoseLandmark.LEFT_ELBOW].y*h)
-            coEX = int(points.landmark[pose.PoseLandmark.LEFT_ELBOW].x*w)
+            
             
 
 
@@ -341,21 +339,16 @@ def flexao():
 
             # Coordenadas de cada ponto dos pés e mãos
             peDY = int(points.landmark[pose.PoseLandmark.RIGHT_FOOT_INDEX].y*h)
+
             peDX = int(points.landmark[pose.PoseLandmark.RIGHT_FOOT_INDEX].x*w)
+
             peEY = int(points.landmark[pose.PoseLandmark.LEFT_FOOT_INDEX].y*h)
+
             peEX = int(points.landmark[pose.PoseLandmark.LEFT_FOOT_INDEX].x*w)
-            quDY = int(points.landmark[pose.PoseLandmark.RIGHT_HIP].y*h)
-            quDX = int(points.landmark[pose.PoseLandmark.RIGHT_HIP].x*w)
-            quEY = int(points.landmark[pose.PoseLandmark.LEFT_HIP].y*h)
-            quEX = int(points.landmark[pose.PoseLandmark.LEFT_HIP].x*w)
+
+            
             omDY = int(points.landmark[pose.PoseLandmark.RIGHT_SHOULDER].y*h)
-            omDX = int(points.landmark[pose.PoseLandmark.RIGHT_SHOULDER].x*w)
-            omEY = int(points.landmark[pose.PoseLandmark.LEFT_SHOULDER].y*h)
-            omDX = int(points.landmark[pose.PoseLandmark.LEFT_SHOULDER].x*w)
-            coDY = int(points.landmark[pose.PoseLandmark.RIGHT_ELBOW].y*h)
-            coDX = int(points.landmark[pose.PoseLandmark.RIGHT_ELBOW].x*w)
-            coEY = int(points.landmark[pose.PoseLandmark.LEFT_ELBOW].y*h)
-            coEX = int(points.landmark[pose.PoseLandmark.LEFT_ELBOW].x*w)
+            
             cbEY = int(points.landmark[pose.PoseLandmark.LEFT_EAR].x*w)
             
 
@@ -378,6 +371,136 @@ def flexao():
                  # Mais de um polichinelo
 
             if omDY <= 700 and distCABECA >= 40:
+                check = True # Quando o ultimo movimento é feito altera a variável para true
+             # Para permitir que outro polichinelo seja finalizado e computado
+
+
+        
+
+            if keyboard.is_pressed('esc'): ### Caso esc seja pressionado, o código encerrará
+                break           
+
+            
+
+
+            # Aqui, criamos o retangulo que vai armazenar o texto que diz quantos
+            # Polichinelos o usuário fez
+
+            texto = f'QTD {contador}'
+
+
+
+            ### Função que cria a forma
+            # De um retângulo no video
+            cv2.rectangle(img,(20,240),(280,120),(255,0,0),-1)
+
+            ### Função que cria a forma
+            # De um retângulo no video
+
+            # Função para adicionar um texto a área de informações	
+            cv2.putText(img,texto,(40,200),cv2.FONT_HERSHEY_SIMPLEX,2,(255,255,255),5)
+            
+
+
+
+        cv2.imshow('Resultado',img)
+        cv2.waitKey(40)
+
+
+def rosca_direta():
+
+    video = cv2.VideoCapture('C:\\Users\\Usuario\\Desktop\\html\\Aula-Jonas\\reconhecedorDePolichinelos\\Contador_Polichinelos\\rosca_direta.mp4')
+
+    contador = 0
+    check = True
+    
+    while True: # Loop para rodar o video
+        
+
+
+        # Variáveis que vão receber o que é reconhecido pela webcam
+        # !!! A primeira é apenas um teste, para verificar se ela está aberta ou não
+        success,img = video.read()
+
+
+        # A variável videoRGB recebe o video com a função cvtColor aplicada nele
+        # Essa função tem a capacidade de alterar o padrão de cores do video/imagem
+        # O opencv Não usa o padrão RGB, então normalmente essa função é usada para
+        # A conversão de uma imagem opencv2 para uma imagem RGB
+        videoRGB = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+
+
+        # Essa variável vai armazenar o resultado do processamento do nosso video
+        # Após o computador verificar se realmente existe um corpo no video
+        # Vai fazer a ligação entre os pontos que estão configurados na rede neural
+        # Devemos notar que para este código, não precisamos baixar nenhuma rede neural
+        # Previamente, pois a biblioteca media pipe faz uma ligação com a api do google
+        # Que devolve as informações necessárias para o funcionamento do código
+        results = Pose.process(videoRGB)
+
+        # A variável points vai armazenar o atributo pose_landmarks 
+        # do objeto, ou variável results, no caso, aqueles pontinhos que marcam cada região
+        # do corpo
+        points = results.pose_landmarks
+
+        # Esse método nós usamos para desenhar as ligações entre os pontos
+        # Armazenados na variável points
+        draw.draw_landmarks(img,points,pose.POSE_CONNECTIONS)
+
+
+        # Extraindo as dimensões da imagem
+        h,w,_ = img.shape
+
+
+
+        if points: # condição que vai verificar se a variavel points não está vazia
+            # caso a variável esteja vazia ele não tentará encontrar os pontos
+
+
+            # Coordenadas de cada ponto dos pés e mãos
+            
+            omDY = int(points.landmark[pose.PoseLandmark.RIGHT_SHOULDER].y*h)
+        
+            omEY = int(points.landmark[pose.PoseLandmark.LEFT_SHOULDER].y*h)
+
+            moDY = int(points.landmark[pose.PoseLandmark.RIGHT_INDEX].y*h)
+
+            moEY = int(points.landmark[pose.PoseLandmark.LEFT_INDEX].y*h)
+
+            cocxDY = int(points.landmark[pose.PoseLandmark.RIGHT_HIP].y*h)
+
+            cocxEY = int(points.landmark[pose.PoseLandmark.LEFT_HIP].y*h)
+
+    
+            
+            
+
+        
+
+
+            # Função utilizada para extrair as cordenadas finais dos pés e mãos 
+            distOMBROD = math.hypot(omDY)
+            distOMBROE = math.hypot(omEY)
+            distMODY = math.hypot(moDY)
+            distMOEY = math.hypot(moEY)
+            distCOCXDY = math.hypot(cocxDY)
+            distCOCXEY = math.hypot(cocxEY)
+
+            print(f'mãos {distMODY}, {distMOEY} ombros {distOMBROD}, {distOMBROE}')
+            
+
+            # maos <=150 pes >=150
+
+
+            # Condicionais para verificar as distâncias descrevidas a cima
+            # E verificar se os polichinelos foram executados com base
+            # Na distância das mãos e dos pés
+            if check == True and distMODY <= distOMBROD and distMOEY <= distOMBROE:
+                contador +=1
+                check = False # Alterando a variável check para falsa para não contar
+                 # Mais de um polichinelo
+
+            if distMODY >= distCOCXDY and distMOEY >= distCOCXEY:
                 check = True # Quando o ultimo movimento é feito altera a variável para true
              # Para permitir que outro polichinelo seja finalizado e computado
 
